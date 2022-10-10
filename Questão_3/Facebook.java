66473538610048026135c1c2517279efb7af97a1
Giovanna Vieira <108762029+voigvie@users.noreply.github.com>
2022-10-09 23:31:48 -0300

    Create Facebook.java
    Resolvendo questão 3

---
+++ Questão_3/Facebook.java
@@ -0,0 +1,10 @@
+package primeiraquestao;
+
+public class Facebook extends Plataforma {
+
+    @Override
+    public void logar() {
+        System.out.println("Logando com Facebook");
+    }
+    
+}
