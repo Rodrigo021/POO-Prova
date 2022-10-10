d788105032150ada6c576fc9e4feaec2991fca77
Rodrigo <rodrigo.srj@hotmail.com>
2022-10-09 21:30:25 -0300

    editando classes

--- Questão_1/Compra.java
+++ Questão_1/Compra.java
@@ -3,10 +3,26 @@ package Questão_1;
 import java.util.ArrayList;
 
 public class Compra {
-    ArrayList<Produto> carrinho = new ArrayList<>();
+    private ArrayList<Produto> carrinho;
+    private double total_produto;
 
     public void add_Produto(Produto produto) {
         carrinho.add(produto);
     }
+
+    public void valor_Compra() {
+        for (Produto produto : carrinho) {
+            total_produto += produto.valor_total();
+        }
+    }
+
+    public ArrayList<Produto> getCarrinho() {
+        return carrinho;
+    }
+
+    public double getTotal_produto() {
+        return total_produto;
+    }
     
+
 }
