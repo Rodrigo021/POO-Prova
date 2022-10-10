efcb3024e22db277ef26f1c70d919fec56bfcf3b
Giovanna Vieira <108762029+voigvie@users.noreply.github.com>
2022-10-09 23:35:22 -0300

    Create Formulario.java
    Resolvendo questão 3

---
+++ Questão_3/Formulario.java
@@ -0,0 +1,19 @@
+package primeiraquestao;
+
+public class Formulario implements Login {
+    protected String email;
+    protected String senha;
+
+    public Formulario(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    @Override
+    public void logar() {
+        System.out.println("Logando com formulário, com o email: " + email);
+        System.out.println("e a senha: " + senha);
+    }
+    
+    
+}
