7b7d537bcfcd16cf12e87ab977fcc016b331ffc4
Giovanna Vieira <108762029+voigvie@users.noreply.github.com>
2022-10-09 23:32:26 -0300

    Create ICloud.java
    Resolvendo questão 3

---
+++ Questão_3/ICloud.java
@@ -0,0 +1,10 @@
+package primeiraquestao;
+
+public class ICloud extends Plataforma {
+       
+
+    @Override
+    public void logar() {
+        System.out.println("Logando com sua conta iCloud");
+    }
+}
