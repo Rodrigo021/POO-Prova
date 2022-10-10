9a0dcb861da18f82c72ad40b51d7681d3354fa06
Giovanna Vieira <108762029+voigvie@users.noreply.github.com>
2022-10-09 23:30:48 -0300

    Create App.java
    Resolvendo questão 3

---
+++ Questão_3/App.java
@@ -0,0 +1,16 @@
+package primeiraquestao;
+
+public class App {
+
+    public static void main(String[] args) {
+        Login loginForms = new Formulario("giovanna_almeida@arapiraca.ufal.br", "senha123"); 
+        loginForms.logar();
+
+        Login loginGoogle = new Google();
+        loginGoogle.logar();
+        
+        Login loginFacebook = new Facebook();
+        loginFacebook.logar();
+    }
+ 
+}
