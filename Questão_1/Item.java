8e570a85753647bde7b5c487d812e867b12b004f
Giovanna Vieira <108762029+voigvie@users.noreply.github.com>
2022-10-09 21:10:15 -0300

    Update Item.java

--- Questão_1/Item.java
+++ Questão_1/Item.java
@@ -2,23 +2,24 @@ package Questão_1;
 
 public class Item {
     private String nome;
-    private double valor;
+    private double preco;
 
-    public Item(String nome, double valor) {
+    public Item(String nome, double preco) {
         this.nome = nome;
-        this.valor = valor;
+        this.preco = preco;
     }
 
     public String getNome() {
         return nome;
     }
-    public double getValor() {
-        return valor;
+    public double getPreco() {
+        return preco;
     }
 
     
-    public String to_String(){
-        return "Nome do Item: "+nome+"\nValor do Item: "+valor;
+    @Override
+    public String toString() {
+        return "Item = " + "nome: " + nome + ", Preço: " + preco + '}';
     }
     
 }
