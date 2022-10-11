083a8842c5da488eca54b2a971c41a38024277d9
Giovanna Vieira <108762029+voigvie@users.noreply.github.com>
2022-10-11 12:31:30 -0300

    Corrigindo métodos

--- Questão_6/ContaEspecial.java
+++ Questão_6/ContaEspecial.java
@@ -8,18 +8,17 @@ public class ContaEspecial extends ContaCorrente {
     
     @Override
     public boolean sacar(double v){
-     if (saldo - v + limite < 0 ){
+     if (saldo + limite  - v < 0 ){
             System.out.println("Ação Inválida.");
             return false;
         } else {
-            if (this.getSaldo() >= v) {
-                this.setSaldo(this.getSaldo() - v);
-                System.out.println("Saque realizado com sucesso.");
-                return true;
-            } else {
-                System.out.println("Saldo Insuficiente");
-                return false;
-            }
+         if (v <= limite){
+             System.out.println("Saque realizado com sucesso.");
+             return true;
+         } else {
+             System.out.println("Saldo insuficiente.");
+             return false;
+         }
         }
     }
 
