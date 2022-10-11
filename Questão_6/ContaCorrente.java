091d43eac96e466ddb5748ad120f5d5ec4299219
Giovanna Vieira <108762029+voigvie@users.noreply.github.com>
2022-10-11 12:30:53 -0300

    Corrigindo métodos

--- Questão_6/ContaCorrente.java
+++ Questão_6/ContaCorrente.java
@@ -22,12 +22,13 @@ public class ContaCorrente {
             System.out.println("Ação Inválida");
         } else {
             this.setSaldo(v + this.getSaldo());  
+            System.out.println("Deposito realizado com sucesso");
         }
     }
 
     protected boolean sacar(double v) {
         if (saldo - v < 0) {
-            System.out.println("Acão Inválida.");
+            System.out.println("Ação Inválida.");
             return false;
         } else {
             if (this.getSaldo() >= v) {
