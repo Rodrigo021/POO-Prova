6d334081db239912afcc819ee12826b6c28862cc
Giovanna Vieira <108762029+voigvie@users.noreply.github.com>
2022-10-11 12:33:09 -0300

    Corrigindo e finalizando questão 6

--- Questão_6/Banco.java
+++ Questão_6/Banco.java
@@ -1,14 +1,21 @@
+import java.util.ArrayList;
+
 public class Banco {
     public static void main(String[] args) {
         ArrayList<ContaCorrente> contas = new ArrayList<>();
-        contas.add(new ContaCorrente(1212));
-        contas.add(new ContaEspecial(1212, 500));
+        contas.add(new ContaCorrente(2210));
+        contas.add(new ContaEspecial(20, 400));
+        Cliente cliente = new Cliente("Giovanna", contas);
         
-        Cliente cliente1 = new Cliente("Rodrigo", contas);
-        Cliente cliente2 = new Cliente("Giovanna", contas);
         
-        System.out.println(cliente1.getContas());
-        System.out.println(cliente2.getContas());
-        
+        System.out.println(cliente.toString());
+        System.out.println(cliente.getContas());
+        contas.get(0).sacar(200);
+        System.out.println("Novo saldo: " + contas.get(0).getSaldo());
+        contas.get(0).depositar(100);
+        System.out.println("Novo saldo: " + contas.get(0).getSaldo());        
+        contas.get(1).sacar(500);
+
+
     } 
 }
