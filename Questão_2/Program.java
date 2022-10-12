270f8292a171d7356362192a5c664089c2964ba6
Rodrigo <rodrigo.srj@hotmail.com>
2022-10-12 16:05:02 -0300

    finalização da questão 2

--- Questão_2/Program.java
+++ Questão_2/Program.java
@@ -3,8 +3,8 @@ package Questão_2;
 public class Program {
     public static void main(String[] args) {
         Estacionamento e = new Estacionamento();
-        e.adicionarCarro(new Carro("Fiat Marea Turbo", 3, 4));
-        e.adicionarCarro(new Carro("Ferrari", 4, 4));
+        e.adicionarCarro(new Carro("Volkswagen Gol", 4, 5));
+        e.adicionarCarro(new Carro("Dogde Hellcat", 1, 2));
         e.listar_Carros();
         System.out.println("\nPessoas no estacionamento: " + e.getpessoasEstacionamento());
 
